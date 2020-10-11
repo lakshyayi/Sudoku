80337f5fdea6c2b7886c4c6a391517ae181cfa19
zhangjl <lakshyali@live.com>
2020-10-11 11:39:23 +0800

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Lakshya on 2020/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
