5410a11264b05c1aed2c91e589959e21bc3238e1
zhangjl <lakshyali@live.com>
2020-10-27 21:01:51 +0800

    a

--- Sudoku/BoardView.swift
+++ Sudoku/BoardView.swift
@@ -19,74 +19,80 @@ struct BoardView: View {
 
     }
     var body: some View {
-        VStack(alignment: .center, spacing: 0){
-            ForEach(self.board.indices , id: \.self){ i in
-                if(i%3==0){
-                    Divider().background(Color.black)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
-                }
-                HStack(alignment: .center, spacing: 0){
-                    ForEach(self.board[i].indices , id: \.self){ j in
-                        if(j%3==0){
-                            Divider().background(Color.black).border(Color.black, width: 2)
-                        }
-                        if self.board[i][j] == nil{
-                            Button(action:{
-                                self.showKeyboard=true
-                            },
-                            label: {
-                                Text(String(" ")).font(.system(size: 18))
-                                                .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                                .background(
-                                                    Rectangle()
-                                                        .fill(Color.blue.opacity(0.2))
-                                                        .frame(width: self.size, height: self.size)
-                                                        .border(Color.gray, width: 0.5)
-                                        
-                                            )
-                                
-                            })
-                        }else{
-                            Text(String(self.board[i][j]!))
-                                .font(.system(size: 18))
-                                .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .background(
-                                    Rectangle()
-                                        .fill(Color.blue.opacity(0.2))
-                                        .frame(width: self.size, height: self.size)    .border(Color.gray, width: 0.5))
-                        }
-               
+        ZStack{
+            Color.white
+            VStack(alignment: .center, spacing: 0){
+                ForEach(self.board.indices , id: \.self){ i in
+                    if(i%3==0){
+                        Divider().background(Color.black)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
                     }
-             
-                }
-            }
-        }.frame(width: self.size*9, height:  self.size*9, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(3)
-        .border(Color.black.opacity(0.7), width: 3)
-        if $showKeyboard.wrappedValue{
-            ZStack{
-                Color.white
-                VStack(alignment: .center, spacing: 0){
-                    ForEach(1...3 , id: \.self){ i in
-                        HStack(alignment: .center, spacing: 0){
-                            ForEach(1...3 , id: \.self){ j in
-                                Text(String(i*3+j-3))
+                    HStack(alignment: .center, spacing: 0){
+                        ForEach(self.board[i].indices , id: \.self){ j in
+                            if(j%3==0){
+                                Divider().background(Color.black).border(Color.black, width: 2)
+                            }
+                            if self.board[i][j] == nil{
+                                Button(action:{
+                                    self.showKeyboard=true
+                                },
+                                label: {
+                                    Text(String(" ")).font(.system(size: 18))
+                                                    .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                                    .background(
+                                                        Rectangle()
+                                                            .fill(Color(hex:0xA193E1).opacity(0.2))
+                                                            .frame(width: self.size, height: self.size)
+                                                            .border(Color.gray, width: 0.5)
+                                            
+                                                )
+                                    
+                                })
+                            }else{
+                                Text(String(self.board[i][j]!))
                                     .font(.system(size: 18))
                                     .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                     .background(
                                         Rectangle()
-                                            .fill(Color.blue.opacity(0.2))
+                                            .fill(Color(hex:0xA193E1).opacity(0.3))
                                             .frame(width: self.size, height: self.size)    .border(Color.gray, width: 0.5))
-                       
                             }
-                     
+                   
                         }
-                        
+                 
                     }
                 }
-            }.frame(width: self.size*3, height:  self.size*4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .cornerRadius(10).shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            }.frame(width: self.size*9, height:  self.size*9, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(3)
             .border(Color.black.opacity(0.7), width: 3)
+            if $showKeyboard.wrappedValue{
+                ZStack{
+                    Color.white
+                    VStack(alignment: .center, spacing: 0){
+                        ForEach(1...3 , id: \.self){ i in
+                            HStack(alignment: .center, spacing: 0){
+                                ForEach(1...3 , id: \.self){ j in
+                                    Text(String(i*3+j-3))
+                                        .font(.system(size: 18))
+                                        .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                        .background(
+                                            Rectangle()
+                                                .fill(Color.blue.opacity(0.2))
+                                                .frame(width: self.size, height: self.size)    .border(Color.gray, width: 0.5))
+                           
+                                }
+                         
+                            }
+                            
+                        }
+                    }
+                }.frame(width: self.size*3, height:  self.size*4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .cornerRadius(10).shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .border(Color.black.opacity(0.7), width: 3)
+                .position(x: 100, y: 100)
+            }
         }
+        
+       
       
     }
 
