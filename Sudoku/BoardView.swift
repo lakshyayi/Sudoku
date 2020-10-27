262abf6bba228efb273dc8b6f05e312e93330045
zhangjl <lakshyali@live.com>
2020-10-27 16:44:30 +0800

    aa

--- Sudoku/BoardView.swift
+++ Sudoku/BoardView.swift
@@ -11,6 +11,7 @@ struct BoardView: View {
     var sudoku:Sudoku
     var board:[[Int?]]
     var size:CGFloat
+    @State private  var showKeyboard = false
     init(){
         self.sudoku = Sudoku(digets:3)
         self.board = sudoku.generate()!
@@ -30,15 +31,21 @@ struct BoardView: View {
                             Divider().background(Color.black).border(Color.black, width: 2)
                         }
                         if self.board[i][j] == nil{
-                            Text(String(" "))
-                            .font(.system(size: 18))
-                                .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .background(
-                                    Rectangle()
-                                        .fill(Color.blue.opacity(0.2))
-                                        .frame(width: self.size, height: self.size)
-                                        .border(Color.gray, width: 0.5)
-                                )
+                            Button(action:{
+                                self.showKeyboard=true
+                            },
+                            label: {
+                                Text(String(" ")).font(.system(size: 18))
+                                                .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                                .background(
+                                                    Rectangle()
+                                                        .fill(Color.blue.opacity(0.2))
+                                                        .frame(width: self.size, height: self.size)
+                                                        .border(Color.gray, width: 0.5)
+                                        
+                                            )
+                                
+                            })
                         }else{
                             Text(String(self.board[i][j]!))
                                 .font(.system(size: 18))
@@ -55,6 +62,32 @@ struct BoardView: View {
             }
         }.frame(width: self.size*9, height:  self.size*9, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(3)
         .border(Color.black.opacity(0.7), width: 3)
+        if $showKeyboard.wrappedValue{
+            ZStack{
+                Color.white
+                VStack(alignment: .center, spacing: 0){
+                    ForEach(1...3 , id: \.self){ i in
+        
+                        HStack(alignment: .center, spacing: 0){
+                            ForEach(1...3 , id: \.self){ j in
+                                
+                                Text(String(i*3+j-3))
+                                    .font(.system(size: 18))
+                                    .frame(width: self.size, height: self.size, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .background(
+                                        Rectangle()
+                                            .fill(Color.blue.opacity(0.2))
+                                            .frame(width: self.size, height: self.size)    .border(Color.gray, width: 0.5))
+                       
+                            }
+                     
+                        }
+                        
+                    }
+                }.frame(width: self.size*3, height:  self.size*4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(3)
+                .border(Color.black.opacity(0.7), width: 3)
+            }
+        }
       
     }
 
